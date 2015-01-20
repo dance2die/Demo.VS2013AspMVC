e04d3b44a1e4507befa15e27e181114a59053ce8
dance2die <dontbothermewithspam@gmail.com>
2015-01-19 23:26:13 -0500

    Initial Import

---
+++ VS2013AspMVCDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VS2013AspMVCDemo.MvcApplication" Language="C#" %>
---
+++ VS2013AspMVCDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VS2013AspMVCDemo.Startup))]
+namespace VS2013AspMVCDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
